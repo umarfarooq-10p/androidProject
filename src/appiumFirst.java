463de0fba319c6ce10d045b7ecf08ce2db47f0d0
Umar Farooq <umar.farooq@tenpearls.com>
2022-09-19 17:23:23 +0500

    Remove unused code

--- src/appiumFirst.java
+++ src/appiumFirst.java
@@ -2,7 +2,6 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
